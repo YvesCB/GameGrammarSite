3c34108ab6f2eb9cfc0a7f69c77838a2a7ccadbb
Yves <33640353+YvesCB@users.noreply.github.com>
2022-04-27 09:13:26 +0200

    Updated countdown until 24.12.23

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 // Set the date we're counting down to
-var countDownDateSun = new Date("Dec 19, 2021 17:00:00").getTime();
-var countDownDateWed = new Date("Dec 22, 2021 18:00:00").getTime();
-var countDownDateFri = new Date("Dec 24, 2021 18:00:00").getTime();
+var countDownDateSun = new Date("Dec 24, 2023 18:00:00").getTime();
+var countDownDateWed = new Date("Dec 24, 2023 18:00:00").getTime();
+var countDownDateFri = new Date("Dec 24, 2023 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
